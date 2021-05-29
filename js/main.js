687891abffa4e3401285b7282e9964c7bfc9edef
Alexander <tipkrut9@gmail.com>
2021-05-29 21:57:38 +0200

    Третьи правки в задании

--- js/main.js
+++ js/main.js
@@ -1,10 +1,7 @@
 function getRandomInRange(min, max) {
   if (typeof(min) === 'number' && typeof(max) === 'number') {
     if (min >= 0 && max > 0) {
-      if (min < max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-      }
-      return 'min больше или равен max';
+      return (min < max) ? Math.floor(Math.random() * (max - min + 1)) + min : 'min больше или равен max';
     }
     return 'min или max не является положительным числом';
   }
@@ -12,10 +9,7 @@ function getRandomInRange(min, max) {
 }
 
 function checkMaxLengthString(string, length) {
-  if (string.length < length) {
-    return true;
-  }
-  return false;
+  return string.length < length;
 }
 
 getRandomInRange(0, 10);

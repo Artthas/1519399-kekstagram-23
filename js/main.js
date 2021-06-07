0fa743d4fa22274a8e90534a7c021391d87b1af4
Alexander <tipkrut9@gmail.com>
2021-06-07 15:45:51 +0200

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -14,3 +14,62 @@ function checkMaxLengthString(string, length) {
 
 getRandomInRange(0, 10);
 checkMaxLengthString('Привет', 10);
+
+const ID = [];
+const URL = [];
+const DESCRIPTION = ['акула', 'кот', 'собака', 'лес', 'море', 'тайга', 'кит', 'бурундук'];
+const LIKES = [];
+const COMMENTS_NAME = ['Александр', 'Владимир', 'Владислав', 'Андрей', 'Алексей', 'Сергей', 'Роман'];
+const COMMENTS_AVATAR = [];
+const COMMENTS_MESSAGE = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+for (let i = 0; ID.length < 25; i++) {
+  ID[i] = i + 1;
+  URL[i] = 'photos/${i + 1}.jpg';
+}
+for (let i = 0; LIKES.length < 200; i++) {
+  LIKES[i] = i + 15;
+}
+for (let i = 0; COMMENTS_AVATAR.length < 6; i++) {
+  COMMENTS_AVATAR[i] = 'img/avatar-${i + 1}.svg';
+}
+
+const getRandomArrayElement = (elements) => {
+  let indexArray = elements[getRandomInRange(0, elements.length - 1)];
+  elements.splice(indexArray, 1);
+  return indexArray;
+};
+
+const createPhoto = () => {
+  let copiedId = ID.slice();
+  let copiedUrl = URL.slice();
+  let copiedDescription = DESCRIPTION.slice();
+  let copiedLikes = LIKES.slice();
+  let copiedCommentsAvatar = COMMENTS_AVATAR.slice();
+  let copiedCommentsMessage = COMMENTS_MESSAGE.slice();
+  let copiedCommentsName = COMMENTS_NAME.slice();
+
+  return {
+    id: getRandomArrayElement(copiedId),
+    url: getRandomArrayElement(copiedUrl),
+    description: getRandomArrayElement(copiedDescription),
+    likes: getRandomArrayElement(copiedLikes),
+    comments: {
+      id: Math.floor(Math.random()),
+      avatar: getRandomArrayElement(copiedCommentsAvatar),
+      message: getRandomArrayElement(copiedCommentsMessage),
+      name: getRandomArrayElement(copiedCommentsName),
+    },
+  };
+};
+
+const PHOTOS_AMOUNT = 25;
+
+const similarPhotos = new Array(PHOTOS_AMOUNT).fill(null).map(() => createPhoto());

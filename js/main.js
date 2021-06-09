07a5f4d699e12287db64d20856497c692d849a5e
Alexander <tipkrut9@gmail.com>
2021-06-09 17:57:20 +0200

    Вторые исправления в больше деталей

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+const descriptions = ['акула', 'кот', 'собака', 'лес', 'море', 'тайга', 'кит', 'бурундук'];
+const commentNames = ['Александр', 'Владимир', 'Владислав', 'Андрей', 'Алексей', 'Сергей', 'Роман'];
+const commentAvatars = [];
+const COMMENT_MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+const PHOTOS_AMOUNT = 25;
+
 function getRandomInRange(min, max) {
   if (typeof(min) === 'number' && typeof(max) === 'number') {
     if (min >= 0 && max > 0) {
@@ -15,63 +28,34 @@ function checkMaxLengthString(string, length) {
 getRandomInRange(0, 10);
 checkMaxLengthString('Привет', 10);
 
-const ID = [];
-const URL = [];
-const DESCRIPTION = ['акула', 'кот', 'собака', 'лес', 'море', 'тайга', 'кит', 'бурундук'];
-const LIKES = [];
-const COMMENTS_NAME = ['Александр', 'Владимир', 'Владислав', 'Андрей', 'Алексей', 'Сергей', 'Роман'];
-const COMMENTS_AVATAR = [];
-const COMMENTS_MESSAGE = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
-];
-
-for (let i = 0; ID.length < 25; i++) {
-  ID[i] = i + 1;
-  URL[i] = `photos/${i + 1}.jpg`;
-}
-for (let i = 0; LIKES.length < 200; i++) {
-  LIKES[i] = i + 15;
-}
-for (let i = 0; COMMENTS_AVATAR.length < 6; i++) {
-  COMMENTS_AVATAR[i] = `img/avatar-${i + 1}.svg`;
+for (let i = 0; commentAvatars.length < 6; i++) {
+  commentAvatars[i] = `img/avatar-${i + 1}.svg`;
 }
 
 const getRandomArrayElement = (elements) => {
   const indexArray = elements[getRandomInRange(0, elements.length - 1)];
-  elements.splice(indexArray, 1);
   return indexArray;
 };
 
-const createPhoto = () => {
-  const copiedId = ID.slice();
-  const copiedUrl = URL.slice();
-  const copiedDescription = DESCRIPTION.slice();
-  const copiedLikes = LIKES.slice();
-  const copiedCommentsAvatar = COMMENTS_AVATAR.slice();
-  const copiedCommentsMessage = COMMENTS_MESSAGE.slice();
-  const copiedCommentsName = COMMENTS_NAME.slice();
+const createComment = (j) => ({
+  id: j + 1,
+  avatar: `img/avatar-${getRandomInRange(1, 6)}.svg`,
+  message: getRandomArrayElement(COMMENT_MESSAGES),
+  name: getRandomArrayElement(commentNames),
+});
 
+const createPhoto = (i) => {
+  const number = 6;
+  const comments = new Array(number).fill(null).map((element, j) => createComment(j));
   return {
-    id: getRandomArrayElement(copiedId),
-    url: getRandomArrayElement(copiedUrl),
-    description: getRandomArrayElement(copiedDescription),
-    likes: getRandomArrayElement(copiedLikes),
-    comments: {
-      id: Math.floor(Math.random()),
-      avatar: getRandomArrayElement(copiedCommentsAvatar),
-      message: getRandomArrayElement(copiedCommentsMessage),
-      name: getRandomArrayElement(copiedCommentsName),
-    },
+    id: i + 1,
+    url: `photos/${i + 1}.jpg`,
+    description: getRandomArrayElement(descriptions),
+    likes: getRandomInRange(15, 200),
+    comments,
   };
 };
 
-const PHOTOS_AMOUNT = 25;
+const similarPhotos = new Array(PHOTOS_AMOUNT).fill(null).map((element, i) => createPhoto(i));
 
-const similarPhotos = new Array(PHOTOS_AMOUNT).fill(null).map(() => createPhoto());
-
-similarPhotos();
+similarPhotos;

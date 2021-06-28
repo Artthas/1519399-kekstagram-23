6346ded511dcb42b2c003cce7c571d09622414b0
Alexander <tipkrut9@gmail.com>
2021-06-28 16:49:32 +0200

    Третьи исправления в Правда или действие (часть 1)

--- js/form.js
+++ js/form.js
@@ -1,38 +1,42 @@
 import {isEscEvent} from './util.js';
 
+const CLASS_HIDDEN = 'hidden';
+const CLASS_MODAL_OPEN = 'modal-open';
+const ERROR_MESSAGES = [
+  'нельзя указать больше пяти хэш-тегов',
+  'один и тот же хэш-тег не может быть использован дважды',
+  'хэш-тег начинается с символа # (решётка)\nстрока после решётки должна состоять из букв и чисел и не может содержать пробелы, спецсимволы (#, @, $ и т. п.), символы пунктуации (тире, дефис, запятая и т. п.), эмодзи и т. д.',
+  'длина комментария не может составлять больше 140 символов',
+];
 const imgUploadInput = document.querySelector('#upload-file');
 const imgUploadOverlay = document.querySelector('.img-upload__overlay');
 const imgUploadCancel = document.querySelector('#upload-cancel');
-const classHidden = 'hidden';
-const classModalOpen = 'modal-open';
+const textHashtags = document.querySelector('.text__hashtags');
+const textDescription = document.querySelector('.text__description');
+
+const closeImgUpload = () => {
+  imgUploadOverlay.classList.add(CLASS_HIDDEN);
+  document.body.classList.remove(CLASS_MODAL_OPEN);
+  imgUploadInput.value = '';
+};
 
 const onUserModalEscKeydown = (evt) => {
   if (isEscEvent(evt)) {
-    if (document.hasFocus()) {
+    if (textHashtags.hasFocus() || textDescription.hasFocus()) {
       return;
     }
     evt.preventDefault();
-    imgUploadOverlay.classList.add(classHidden);
-    document.body.classList.remove(classModalOpen);
-    imgUploadInput.value = '';
+    closeImgUpload();
     document.removeEventListener('keydown', onUserModalEscKeydown);
   }
 };
 
-function closeImgUpload () {
-  imgUploadOverlay.classList.add(classHidden);
-  document.body.classList.remove(classModalOpen);
-  imgUploadInput.value = '';
-
-  document.removeEventListener('keydown', onUserModalEscKeydown);
-}
-
-function openImgUpload () {
-  imgUploadOverlay.classList.remove(classHidden);
-  document.body.classList.add(classModalOpen);
+const openImgUpload = () => {
+  imgUploadOverlay.classList.remove(CLASS_HIDDEN);
+  document.body.classList.add(CLASS_MODAL_OPEN);
 
   document.addEventListener('keydown', onUserModalEscKeydown);
-}
+};
 
 imgUploadInput.addEventListener('change', () => {
   openImgUpload();
@@ -40,18 +44,11 @@ imgUploadInput.addEventListener('change', () => {
 
 imgUploadCancel.addEventListener('click', () => {
   closeImgUpload();
+  document.removeEventListener('keydown', onUserModalEscKeydown);
 });
 
 /* Validation */
 
-const textHashtags = document.querySelector('.text__hashtags');
-const textDescription = document.querySelector('.text__description');
-const ERROR_MESSAGES = [
-  'нельзя указать больше пяти хэш-тегов',
-  'один и тот же хэш-тег не может быть использован дважды',
-  'хэш-тег начинается с символа # (решётка)\nстрока после решётки должна состоять из букв и чисел и не может содержать пробелы, спецсимволы (#, @, $ и т. п.), символы пунктуации (тире, дефис, запятая и т. п.), эмодзи и т. д.',
-  'длина комментария не может составлять больше 140 символов',
-];
 const reportAndSetCustomValidity = (message) => {
   textHashtags.setCustomValidity(message);
   textHashtags.reportValidity();

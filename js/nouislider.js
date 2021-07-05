a98ccb1a19c958bd09a2cd4487f7ea84019f0021
Alexander <tipkrut9@gmail.com>
2021-07-05 19:21:46 +0200

    Первые исправления в Помощь друга

--- js/nouislider.js
+++ js/nouislider.js
@@ -1,84 +1,84 @@
 /*! nouislider - 14.6.3 - 11/19/2020 */
 (function(factory) {
-    if (typeof define === "function" && define.amd) {
-        // AMD. Register as an anonymous module.
-        define([], factory);
-    } else if (typeof exports === "object") {
-        // Node/CommonJS
-        module.exports = factory();
-    } else {
-        // Browser globals
-        window.noUiSlider = factory();
-    }
+  if (typeof define === "function" && define.amd) {
+    // AMD. Register as an anonymous module.
+    define([], factory);
+  } else if (typeof exports === "object") {
+    // Node/CommonJS
+    module.exports = factory();
+  } else {
+    // Browser globals
+    window.noUiSlider = factory();
+  }
 })(function() {
-    "use strict";
+  "use strict";
 
-    var VERSION = "14.6.3";
+  var VERSION = "14.6.3";
 
-    //region Helper Methods
+  //region Helper Methods
 
-    function isValidFormatter(entry) {
-        return typeof entry === "object" && typeof entry.to === "function" && typeof entry.from === "function";
+  function isValidFormatter(entry) {
+    return typeof entry === "object" && typeof entry.to === "function" && typeof entry.from === "function";
+  }
+
+  function removeElement(el) {
+    el.parentElement.removeChild(el);
+  }
+
+  function isSet(value) {
+    return value !== null && value !== undefined;
+  }
+
+  // Bindable version
+  function preventDefault(e) {
+    e.preventDefault();
+  }
+
+  // Removes duplicates from an array.
+  function unique(array) {
+    return array.filter(function(a) {
+      return !this[a] ? (this[a] = true) : false;
+    }, {});
+  }
+
+  // Round a value to the closest 'to'.
+  function closest(value, to) {
+    return Math.round(value / to) * to;
+  }
+
+  // Current position of an element relative to the document.
+  function offset(elem, orientation) {
+    var rect = elem.getBoundingClientRect();
+    var doc = elem.ownerDocument;
+    var docElem = doc.documentElement;
+    var pageOffset = getPageOffset(doc);
+
+    // getBoundingClientRect contains left scroll in Chrome on Android.
+    // I haven't found a feature detection that proves this. Worst case
+    // scenario on mis-match: the 'tap' feature on horizontal sliders breaks.
+    if (/webkit.*Chrome.*Mobile/i.test(navigator.userAgent)) {
+      pageOffset.x = 0;
     }
 
-    function removeElement(el) {
-        el.parentElement.removeChild(el);
+    return orientation
+      ? rect.top + pageOffset.y - docElem.clientTop
+      : rect.left + pageOffset.x - docElem.clientLeft;
+  }
+
+  // Checks whether a value is numerical.
+  function isNumeric(a) {
+    return typeof a === "number" && !isNaN(a) && isFinite(a);
+  }
+
+  // Sets a class and removes it after [duration] ms.
+  function addClassFor(element, className, duration) {
+    if (duration > 0) {
+      addClass(element, className);
+      setTimeout(function() {
+        removeClass(element, className);
+      }, duration);
     }
-
-    function isSet(value) {
-        return value !== null && value !== undefined;
-    }
-
-    // Bindable version
-    function preventDefault(e) {
-        e.preventDefault();
-    }
-
-    // Removes duplicates from an array.
-    function unique(array) {
-        return array.filter(function(a) {
-            return !this[a] ? (this[a] = true) : false;
-        }, {});
-    }
-
-    // Round a value to the closest 'to'.
-    function closest(value, to) {
-        return Math.round(value / to) * to;
-    }
-
-    // Current position of an element relative to the document.
-    function offset(elem, orientation) {
-        var rect = elem.getBoundingClientRect();
-        var doc = elem.ownerDocument;
-        var docElem = doc.documentElement;
-        var pageOffset = getPageOffset(doc);
-
-        // getBoundingClientRect contains left scroll in Chrome on Android.
-        // I haven't found a feature detection that proves this. Worst case
-        // scenario on mis-match: the 'tap' feature on horizontal sliders breaks.
-        if (/webkit.*Chrome.*Mobile/i.test(navigator.userAgent)) {
-            pageOffset.x = 0;
-        }
-
-        return orientation
-            ? rect.top + pageOffset.y - docElem.clientTop
-            : rect.left + pageOffset.x - docElem.clientLeft;
-    }
-
-    // Checks whether a value is numerical.
-    function isNumeric(a) {
-        return typeof a === "number" && !isNaN(a) && isFinite(a);
-    }
-
-    // Sets a class and removes it after [duration] ms.
-    function addClassFor(element, className, duration) {
-        if (duration > 0) {
-            addClass(element, className);
-            setTimeout(function() {
-                removeClass(element, className);
-            }, duration);
-        }
-    }
+  }
 
     // Limits a value to 0 - 100
     function limit(a) {
--- js/photo-edit.js
+++ js/photo-edit.js
@@ -3,28 +3,28 @@ const scaleControlBigger = document.querySelector('.scale__control--bigger');
 const scaleControlValue = document.querySelector('.scale__control--value');
 const imgUploadPreview = document.querySelector('.img-upload__preview');
 let scaleNumber = 1;
-scaleControlSmaller.addEventListener('click', () => {
-    if (scaleControlValue.value === '25%') {
-      evt.preventDefault();
-    } else {
-      scaleControlValue.value = parseFloat(scaleControlValue.value);
-      scaleControlValue.value -= 25;
-      scaleControlValue.value += '%';
-      scaleNumber -= 0.25;
-      imgUploadPreview.style = `transform: scale(${scaleNumber})`;
-    }
+scaleControlSmaller.addEventListener('click', (evt) => {
+  if (scaleControlValue.value === '25%') {
+    evt.preventDefault();
+  } else {
+    scaleControlValue.value = parseFloat(scaleControlValue.value);
+    scaleControlValue.value -= 25;
+    scaleControlValue.value += '%';
+    scaleNumber -= 0.25;
+    imgUploadPreview.style = `transform: scale(${scaleNumber})`;
+  }
 });
 
 scaleControlBigger.addEventListener('click', (evt) => {
-    if (scaleControlValue.value === '100%') {
-      evt.preventDefault();
-    } else {
-      scaleControlValue.value = parseFloat(scaleControlValue.value);
-      scaleControlValue.value = Number(scaleControlValue.value) + Number(25);
-      scaleControlValue.value += '%';
-      scaleNumber += 0.25;
-      imgUploadPreview.style = `transform: scale(${scaleNumber})`;
-    }
+  if (scaleControlValue.value === '100%') {
+    evt.preventDefault();
+  } else {
+    scaleControlValue.value = parseFloat(scaleControlValue.value);
+    scaleControlValue.value = Number(scaleControlValue.value) + Number(25);
+    scaleControlValue.value += '%';
+    scaleNumber += 0.25;
+    imgUploadPreview.style = `transform: scale(${scaleNumber})`;
+  }
 });
 
 /* Effects */
@@ -33,36 +33,37 @@ const sliderElement = document.querySelector('.effect-level__slider');
 const imgUploadEffects = document.querySelector('.img-upload__effects');
 const imgUploadPreviewPicture = document.querySelector('.img-upload__preview-picture');
 const effectLevelValue = document.querySelector('.effect-level__value');
-const imgUploadEffectLevel = document.querySelector('.img-upload__effect-level');
-const effectsItemFirst = document.querySelector('.effects__item-first');
-const effectNames = [
+const EFFECT_NAMES = [
   'chrome',
   'sepia',
   'marvin',
   'phobos',
-  'heat'
-]
+  'heat',
+];
+const EFFECT_STYLES = {
+  chrome: { name: 'grayscale', measure: '' },
+  sepia: { name: 'sepia', measure: '' },
+  marvin: { name: 'invert', measure: '%' },
+  phobos: { name: 'blur', measure: 'px' },
+  heat: { name: 'brightness', measure: '' },
+};
 
 imgUploadEffects.addEventListener('change', (evt) => {
-  for (let i = 0; i < effectNames.length; i++) {
-    let effectClass = 'effects__preview--';
-    effectClass += effectNames[i];
-    for (let j = 0; j < imgUploadPreviewPicture.classList.length; j++) {
-      if (imgUploadPreviewPicture.classList[j] === effectClass) {
-        imgUploadPreviewPicture.classList.remove(effectClass);
-        sliderElement.noUiSlider.destroy();
-      }
-    }
+  let options;
+  if (imgUploadPreviewPicture.classList.length > 1) {
+    sliderElement.noUiSlider.destroy();
+    imgUploadPreviewPicture.style = '';
   }
-  for (let i = 0; i < effectNames.length; i++) {
+  imgUploadPreviewPicture.className = 'img-upload__preview-picture';
+  for (let i = 0; i < EFFECT_NAMES.length; i++) {
     let effectClass = 'effects__preview--';
-    if (effectNames[i] === evt.target.value) {
+    if (EFFECT_NAMES[i] === evt.target.value) {
       effectClass += evt.target.value;
       imgUploadPreviewPicture.classList.add(effectClass);
       noUiSlider.create(sliderElement, {
         range: {
-            min: 0,
-            max: 1,
+          min: 0,
+          max: 1,
         },
         start: 1,
         step: 0.1,
@@ -70,54 +71,55 @@ imgUploadEffects.addEventListener('change', (evt) => {
       });
     }
   }
-  if (evt.target.value === effectNames[0] || evt.target.value === effectNames[1]) {
-    sliderElement.noUiSlider.updateOptions({
+  if (evt.target.value === EFFECT_NAMES[0] || evt.target.value === EFFECT_NAMES[1]) {
+    options = {
       range: {
-          min: 0,
-          max: 1,
+        min: 0,
+        max: 1,
       },
-      step: 0.1,
-    });
-  } else if (evt.target.value === effectNames[2]) {
-    sliderElement.noUiSlider.updateOptions({
+    };
+  } else if (evt.target.value === EFFECT_NAMES[2]) {
+    options = {
       range: {
-          min: 0,
-          max: 100,
+        min: 0,
+        max: 100,
       },
       start: 100,
       step: 1,
-    });
-  } else if (evt.target.value === effectNames[3] || evt.target.value === effectNames[4]) {
-    sliderElement.noUiSlider.updateOptions({
+    };
+  } else if (evt.target.value === EFFECT_NAMES[3] || evt.target.value === EFFECT_NAMES[4]) {
+    options = {
       range: {
-          min: 0,
-          max: 3,
+        min: 0,
+        max: 3,
       },
       start: 3,
-      step: 0.1,
+    };
+  }
+
+  if (imgUploadPreviewPicture.classList.length !== 1) {
+    sliderElement.noUiSlider.updateOptions(options);
+    sliderElement.noUiSlider.on('update', (_, handle, unencoded) => {
+      let name;
+      let measure;
+      effectLevelValue.value = unencoded[handle];
+      if (evt.target.value === EFFECT_NAMES[0]) {
+        name = EFFECT_STYLES.chrome.name;
+        measure = EFFECT_STYLES.chrome.measure;
+      } else if (evt.target.value === EFFECT_NAMES[1]) {
+        name = EFFECT_STYLES.sepia.name;
+        measure = EFFECT_STYLES.sepia.measure;
+      } else if (evt.target.value === EFFECT_NAMES[2]) {
+        name = EFFECT_STYLES.marvin.name;
+        measure = EFFECT_STYLES.marvin.measure;
+      } else if (evt.target.value === EFFECT_NAMES[3]) {
+        name = EFFECT_STYLES.phobos.name;
+        measure = EFFECT_STYLES.phobos.measure;
+      } else if (evt.target.value === EFFECT_NAMES[4]) {
+        name = EFFECT_STYLES.heat.name;
+        measure = EFFECT_STYLES.heat.measure;
+      }
+      imgUploadPreviewPicture.style = `filter: ${name}(${effectLevelValue.value}${measure})`;
     });
   }
-  sliderElement.noUiSlider.on('update', (_, handle, unencoded) => {
-    effectLevelValue.value = unencoded[handle];
-    if (evt.target.value === effectNames[0]) {
-      imgUploadPreviewPicture.style = '';
-      imgUploadPreviewPicture.style = `filter: grayscale(${effectLevelValue.value})`;
-    } else if (evt.target.value === effectNames[1]) {
-      imgUploadPreviewPicture.style = '';
-      imgUploadPreviewPicture.style = `filter: sepia(${effectLevelValue.value})`;
-    } else if (evt.target.value === effectNames[2]) {
-      imgUploadPreviewPicture.style = '';
-      imgUploadPreviewPicture.style = `filter: invert(${effectLevelValue.value}%)`;
-    } else if (evt.target.value === effectNames[3]) {
-      imgUploadPreviewPicture.style = '';
-      imgUploadPreviewPicture.style = `filter: blur(${effectLevelValue.value}px)`;
-    } else if (evt.target.value === effectNames[4]) {
-      imgUploadPreviewPicture.style = '';
-      imgUploadPreviewPicture.style = `filter: brightness(${effectLevelValue.value})`;
-    } else {
-      imgUploadPreviewPicture.style = '';
-    }
-  });
 });
-
-effectsItemFirst.removeEventListener('change', () => {});

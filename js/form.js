216371d51714b92799769ba896d70298ea314f96
Alexander <tipkrut9@gmail.com>
2021-07-12 15:03:42 +0200

    Надо подкачаться

--- js/form.js
+++ js/form.js
@@ -1,4 +1,5 @@
 import {isEscEvent} from './util.js';
+import {letNumberOfScale} from './photo-edit.js';
 
 const CLASS_HIDDEN = 'hidden';
 const CLASS_MODAL_OPEN = 'modal-open';
@@ -8,6 +9,7 @@ const ERROR_MESSAGES = [
   'хэш-тег начинается с символа # (решётка)\nстрока после решётки должна состоять из букв и чисел и не может содержать пробелы, спецсимволы (#, @, $ и т. п.), символы пунктуации (тире, дефис, запятая и т. п.), эмодзи и т. д.',
   'длина комментария не может составлять больше 140 символов',
 ];
+const imgUploadForm = document.querySelector('.img-upload__form');
 const imgUploadInput = document.querySelector('#upload-file');
 const imgUploadOverlay = document.querySelector('.img-upload__overlay');
 const imgUploadCancel = document.querySelector('#upload-cancel');
@@ -28,6 +30,16 @@ const closeImgUpload = () => {
   imgUploadOverlay.classList.add(CLASS_HIDDEN);
   document.body.classList.remove(CLASS_MODAL_OPEN);
   imgUploadInput.value = '';
+  document.querySelector('.img-upload__preview-picture').className = 'img-upload__preview-picture';
+  document.querySelector('.img-upload__preview-picture').style = '';
+  document.querySelector('.img-upload__preview').style = '';
+  document.querySelector('.scale__control--value').value = '100%';
+  if (document.querySelector('.effect-level__slider').className !== 'effect-level__slider') {
+    document.querySelector('.effect-level__slider').noUiSlider.destroy();
+  }
+  textHashtags.value = '';
+  textDescription.value = '';
+  letNumberOfScale();
 };
 
 const onUserModalEscKeydown = (evt) => {
@@ -91,3 +103,91 @@ textDescription.addEventListener('blur', () => {
 
   reportAndSetCustomValidity(message, textDescription);
 });
+
+/* Submitting */
+
+const successWindowTemplate = document.querySelector('#success')
+  .content
+  .querySelector('.success');
+const errorWindowTemplate = document.querySelector('#error')
+  .content
+  .querySelector('.error');
+
+const showSuccessWindow = () => {
+  const successWindow = successWindowTemplate.cloneNode(true);
+  document.body.appendChild(successWindow);
+  document.addEventListener('keydown', onSuccessEscKeydown);
+  document.addEventListener('click', onSuccessClickDown);
+}
+const showErrorWindow = () => {
+  const errorWindow = errorWindowTemplate.cloneNode(true);
+  document.body.appendChild(errorWindow);
+  document.addEventListener('keydown', onErrorEscKeydown);
+  document.addEventListener('click', onErrorClickDown);
+}
+const onSuccessEscKeydown = (evt) => {
+  if (isEscEvent(evt)) {
+    evt.preventDefault();
+    document.body.removeChild(document.querySelector('.success'));
+    document.removeEventListener('keydown', onSuccessEscKeydown);
+    document.removeEventListener('click', onSuccessClickDown);
+  }
+};
+const onErrorEscKeydown = (evt) => {
+  if (isEscEvent(evt)) {
+    evt.preventDefault();
+    document.body.removeChild(document.querySelector('.error'));
+    document.removeEventListener('keydown', onErrorEscKeydown);
+    document.removeEventListener('click', onErrorClickDown);
+  }
+};
+const onSuccessClickDown = (evt) => {
+  if (evt.target === document.querySelector('.success__inner')) {
+    evt.preventDefault();
+  } else {
+    document.body.removeChild(document.querySelector('.success'));
+    document.removeEventListener('click', onSuccessClickDown);
+    document.removeEventListener('keydown', onSuccessEscKeydown);
+  }
+}
+const onErrorClickDown = (evt) => {
+  if (evt.target === document.querySelector('.error__inner')) {
+    evt.preventDefault();
+  } else {
+    document.body.removeChild(document.querySelector('.error'));
+    document.removeEventListener('click', onErrorClickDown);
+    document.removeEventListener('keydown', onErrorEscKeydown);
+  }
+}
+
+const setFormSubmit = (onSuccess) => {
+  imgUploadForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+  
+    const formData = new FormData(evt.target);
+  
+    fetch(
+      'https://23.javascript.pages.academy/kekstagram',
+      {
+        method: 'POST',
+        body: formData,
+      },
+    ).then((response) => {
+       if (response.ok) {
+        onSuccess();
+        showSuccessWindow();
+       } else {
+        onSuccess();
+        showErrorWindow();
+       }
+     })
+     .catch(() => {
+       onSuccess();
+       showErrorWindow();
+     })
+
+
+  });
+}
+
+export {setFormSubmit, closeImgUpload};
--- js/photos-generation.js
+++ js/photos-generation.js
@@ -1,20 +1,19 @@
-import {getSimilarPhotos} from './photo.js';
-
 const pictureList = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture')
   .content
   .querySelector('.picture');
 
-const pictureArray = getSimilarPhotos();
+const renderSimilarPhotos = (pictureArray) => {
+  const pictureListFragment = document.createDocumentFragment();
 
-const pictureListFragment = document.createDocumentFragment();
+  pictureArray.forEach(({url, likes, comments}) => {
+    const pictureElement = pictureTemplate.cloneNode(true);
+    pictureElement.querySelector('.picture__img').src = url;
+    pictureElement.querySelector('.picture__likes').textContent = likes;
+    pictureElement.querySelector('.picture__comments').textContent = comments.length;
+    pictureListFragment.appendChild(pictureElement);
+  });
+  pictureList.appendChild(pictureListFragment);
+};
 
-pictureArray.forEach(({url, likes, comments}) => {
-  const pictureElement = pictureTemplate.cloneNode(true);
-  pictureElement.querySelector('.picture__img').src = url;
-  pictureElement.querySelector('.picture__likes').textContent = likes;
-  pictureElement.querySelector('.picture__comments').textContent = comments.length;
-  pictureListFragment.appendChild(pictureElement);
-});
-
-pictureList.appendChild(pictureListFragment);
+export {renderSimilarPhotos};

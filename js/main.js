d2a091e09272f393ca9def21b175a668719a46c3
Alexander <tipkrut9@gmail.com>
2021-07-16 17:08:15 +0200

    Отрисуй меня полностью (часть 2)

--- js/main.js
+++ js/main.js
@@ -3,9 +3,11 @@ import './photos-generation.js';
 import './form.js';
 import './nouislider.js';
 import './photo-edit.js';
+import './photo-full-size.js';
 import {renderSimilarPhotos} from './photos-generation.js';
 import {closeImgUpload} from './form.js';
 import {setFormSubmit} from './form.js';
+import {showPhotoFullSize, picturesContainer} from './photo-full-size.js';
 
 fetch('https://23.javascript.pages.academy/kekstagram/data')
   .then((response) => {
@@ -17,7 +19,9 @@ fetch('https://23.javascript.pages.academy/kekstagram/data')
   })
   .then((photos) => {
     renderSimilarPhotos(photos);
+    picturesContainer.addEventListener('click', showPhotoFullSize(photos));
   })
   .catch();
 
 setFormSubmit(closeImgUpload);
+

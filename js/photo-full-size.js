992922591a12404611c36971aa8dee5e7e0e1499
Alexander <tipkrut9@gmail.com>
2021-07-17 13:04:45 +0200

    Правда или действие (часть 2)

--- js/photo-full-size.js
+++ js/photo-full-size.js
@@ -6,17 +6,34 @@ const bigPicture = document.querySelector('.big-picture');
 const commentsCount = bigPicture.querySelector('.comments-count');
 const likesCount = bigPicture.querySelector('.likes-count');
 const img = bigPicture.querySelector('img');
-const socialCommentTemplate = bigPicture.querySelector('.social__comment');
 const socialCommentList = bigPicture.querySelector('.social__comments');
 const socialCaption = bigPicture.querySelector('.social__caption');
 const socialCommentCount = bigPicture.querySelector('.social__comment-count');
 const commentsLoader = bigPicture.querySelector('.comments-loader');
+const socialCommentTemplate = document.querySelector('#social__comment')
+  .content
+  .querySelector('.social__comment');
 
+const showMoreComments = () => {
+  const hiddenComments = socialCommentList.querySelectorAll('.hidden');
+  for (let i = 0; i <= 4; i++) {
+    if (socialCommentList.querySelectorAll('.hidden').length === 0) {
+      commentsLoader.classList.add('hidden');
+      break;
+    } else {
+      hiddenComments[i].classList.remove('hidden');
+    }
+  }
+  const number = bigPicture.querySelectorAll('.social__comment').length - socialCommentList.querySelectorAll('.hidden').length;
+  socialCommentCount.textContent = `${number} из ${bigPicture.querySelectorAll('.social__comment').length} комментариев`;
+};
 const closeBigPictureOnClick = () => {
   bigPicture.classList.add('hidden');
   document.body.classList.remove('modal-open');
   const newSocialCommentList = bigPicture.querySelectorAll('.social__comment');
-  for (let i = newSocialCommentList.length - 1; i > 1; i--) {
+  socialCommentCount.textContent = '';
+  commentsLoader.classList.remove('hidden');
+  for (let i = newSocialCommentList.length - 1; i >= 0; i--) {
     socialCommentList.removeChild(newSocialCommentList[i]);
   }
   removeBigPictureListener();
@@ -28,6 +45,7 @@ const closeBigPictureOnKeyDown = (evt) => {
   bigPicture.classList.add('hidden');
   document.body.classList.remove('modal-open');
   const newSocialCommentList = bigPicture.querySelectorAll('.social__comment');
+  socialCommentCount.textContent = '';
   for (let i = newSocialCommentList.length - 1; i > 1; i--) {
     socialCommentList.removeChild(newSocialCommentList[i]);
   }
@@ -44,17 +62,24 @@ const showPhotoFullSize = (photos) => (evt) => {
     socialCaption.textContent = photos[photoNumber].description;
     for (let i = 0; i < photos[photoNumber].comments.length; i++) {
       const socialComment = socialCommentTemplate.cloneNode(true);
+      const socialCommentArray = bigPicture.querySelectorAll('.social__comment');
       socialComment.querySelector('.social__picture').src = photos[photoNumber].comments[i].avatar;
       socialComment.querySelector('.social__picture').alt = photos[photoNumber].comments[i].name;
       socialComment.querySelector('.social__text').textContent = photos[photoNumber].comments[i].message;
+      if (socialCommentArray.length >= 5) {
+        socialComment.classList.add('hidden');
+      }
       socialCommentList.appendChild(socialComment);
     }
-    socialCommentCount.classList.add('hidden');
-    commentsLoader.classList.add('hidden');
+    if (bigPicture.querySelectorAll('.social__comment').length <= 5) {
+      commentsLoader.classList.add('hidden');
+    }
+    socialCommentCount.textContent = `${bigPicture.querySelectorAll('.social__comment').length - socialCommentList.querySelectorAll('.hidden').length} из ${bigPicture.querySelectorAll('.social__comment').length} комментариев`;
     document.body.classList.add('modal-open');
     bigPictureCancel.addEventListener('click', closeBigPictureOnClick);
     document.addEventListener('keydown', closeBigPictureOnKeyDown);
+    commentsLoader.addEventListener('click', showMoreComments);
   }
 };
 
-export {showPhotoFullSize, picturesContainer, bigPictureCancel, closeBigPictureOnClick, closeBigPictureOnKeyDown};
+export {showPhotoFullSize, picturesContainer, bigPictureCancel, closeBigPictureOnClick, closeBigPictureOnKeyDown, commentsLoader, showMoreComments};
